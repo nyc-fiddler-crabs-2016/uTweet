600b8aa655cbc1efc1d2588bc9a917f2ae9d1612
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-03-24 12:32:05 -0400

    Add self-join to differentiae between followers and followed users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   validates :email, :username, presence: true, uniqueness: true
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 
-  has_many :tweets, foreign_key: "poster_id"
+  has_many :tweets, foreign_key: "poster_id", dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id"
   has_many :followers, through: :relationships, source: :follower
-  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followings, foreign_key: "followed_id", class_name: "Relationship"
+  has_many :followed_users, through: :followings, source: :followed
 end
