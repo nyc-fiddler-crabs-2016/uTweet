08e36e06a866b97d2c7a11ef56d0ba3c67422662
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-03-24 12:06:15 -0400

    Add functioning ajax for posting new tweets. Complete controller and views for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  helper_method :current_user, :authorize_user!
+  helper_method :current_user, :authorized?
 
   def current_user
     @user ||= User.find session[:user_id] if session[:user_id]
   end
 
-  def authorize_user!
-    redirect_to new_session_path unless current_user.present?
+  def authorized?(tweet)
+    current_user && (current_user.id == tweet.poster_id)
   end
 end
